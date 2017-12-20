f98c940e83ec4708e4e4e70164ac7244b464fcc5
Robley Gori <robleyadrian@gmail.com>
2017-12-20 10:06:54 +0300

    Initial commit.

---
+++ server/tests/test_data.js
@@ -0,0 +1,33 @@
+// Test data
+module.exports = {
+    sampleUserA: {
+        username: 'sampleUserA',
+        names: 'sample user a',
+        password: 'sampleUserAPassword',
+        email: 'sampleusera@admin.com',
+        bio: 'A sample admin'
+    },
+    sampleUserB: {
+        username: 'sampleUserB',
+        names: 'sample user b',
+        password: 'sampleUserBPassword',
+        email: 'sampleuserb@normal.com',
+        bio: 'A sample normal user'
+    },
+    sampleUserC: {
+        username: 'sampleUserC',
+        names: 'sample user c',
+        password: 'sampleUserCPassword',
+        email: 'sampleuserc@normal.com',
+        bio: 'A sample normal user'
+    },
+    sampleTweetA: {
+        text: 'Sample tweet A'
+    },
+    sampleTweetB: {
+        text: 'Sample tweet B'
+    },
+    sampleTweetC: {
+        text: 'Sample tweet C'
+    }
+};
